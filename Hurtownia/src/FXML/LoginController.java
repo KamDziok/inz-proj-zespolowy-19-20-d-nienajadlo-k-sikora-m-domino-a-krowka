e5e9553355f79dfd372ed1df5a140a60455f9f97
Monika <monia.tyczyn@gmail.com>
2019-11-20 13:00:48 +0100

    Panel logowania - Radio Button

--- Hurtownia/src/FXML/LoginController.java
+++ Hurtownia/src/FXML/LoginController.java
@@ -7,6 +7,7 @@ package FXML;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import com.gluonhq.charm.glisten.control.ToggleButtonGroup;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
@@ -15,6 +16,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 
 /**
  * FXML Controller class
@@ -22,25 +24,28 @@ import javafx.scene.control.TextField;
  * @author monika
  */
 public class LoginController implements Initializable {
-
+    
     @FXML
     private PasswordField passwd;
-
+    
     @FXML
     private TextField login;
-
+    
     @FXML
     private RadioButton klientR;
-
+    
     @FXML
     private RadioButton pracownikR;
-
+    
     @FXML
     private Button zalogujbtn;
-
+    
+    @FXML
+    private ToggleButtonGroup group;
+    
     @FXML
     private Label status;
-
+    
     @FXML
     void LogInAction(ActionEvent event) {
         if (pracownikR.isSelected()) {
@@ -58,21 +63,12 @@ public class LoginController implements Initializable {
             }
         }
     }
-
-    @FXML
-    void Select(ActionEvent event) {
-        if (event.getSource().equals(klientR)) {
-            pracownikR.setSelected(false);
-        } else {
-            klientR.setSelected(false);
-        }
-
-    }
-
+    
     @Override
-    public void initialize(URL url, ResourceBundle rb
-    ) {
-        // TODO
+    public void initialize(URL url, ResourceBundle rb) {
+      final ToggleGroup  group = new ToggleGroup();
+        klientR.setToggleGroup(group);
+        pracownikR.setToggleGroup(group);
     }
-
+    
 }
