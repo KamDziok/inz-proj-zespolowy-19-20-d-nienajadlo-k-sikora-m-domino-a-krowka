955c3d5c51d88d7bbdf07ce915541084abb6a5d4
Dawid Nienajadło <dawidnienajadlo96@gmail.com>
2019-12-19 17:54:08 +0100

    productQuery ready to start writing checkLastId

--- Hurtownia/src/hibernate/ProduktQuery.java
+++ Hurtownia/src/hibernate/ProduktQuery.java
@@ -5,12 +5,21 @@
  */
 package hibernate;
 
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.criterion.Order;
+
 /**
  *
  * @author monika
  */
 public class ProduktQuery {
     
-    
+    Session session = null;
+    Query query = null;
+    Criteria criteria = null;
+
     
 }
