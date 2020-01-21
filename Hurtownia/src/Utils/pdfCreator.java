c24f212318efa9a32e43c390a637f67df17e57bc
Dawid Nienajadło <dawidnienajadlo96@gmail.com>
2020-01-21 20:11:21 +0100

    readHtml class method created

--- Hurtownia/src/Utils/pdfCreator.java
+++ Hurtownia/src/Utils/pdfCreator.java
@@ -1,8 +1,19 @@
 package Utils;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 /**
  *
  * @author dawid
  */
 public class pdfCreator {
     
+    
+    public static String readHtml(String path) throws FileNotFoundException, IOException{
+        String contents = new String(Files.readAllBytes(Paths.get("C:\\Users\\dawid\\Documents\\NetBeansProjects\\pdfCreator\\src\\pdfcreator\\"+path)));
+        return contents;
+    }
 }
