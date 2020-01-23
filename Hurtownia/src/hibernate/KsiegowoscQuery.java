410bbd79aa61b87fd64858a590f6f8d0be2077e2
Adam96 <adamkrowka108@gmail.com>
2020-01-23 14:27:10 +0100

    Księgowość faktura

---
+++ Hurtownia/src/hibernate/KsiegowoscQuery.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hibernate;
+
+import Utils.pdfCreator;
+import com.itextpdf.text.DocumentException;
+import java.io.IOException;
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author adamk
+ */
+public class KsiegowoscQuery {
+    Session session = null;
+    Query query = null;
+    Criteria criteria = null;
+    
+       public void pobierzFakture(String zamowienieID , int vat , String waluta ) 
+           throws DocumentException, IOException{
+        
+       
+       Zamowienie z = null;
+        session = HibernateUtil.getSessionFactory().openSession();
+        String hql = "From Zamowienie WHERE ZamowienieID  = " + zamowienieID + "";
+        query = session.createQuery(hql);
+        z = (Zamowienie) query.uniqueResult();
+        session.close();
+
+        
+        Klient k = null;
+        session = HibernateUtil.getSessionFactory().openSession();
+        String hql2 = "From Klient WHERE KlientID  = '" + z.getKlientID() + "'";
+        query = session.createQuery(hql2);
+        k = (Klient) query.uniqueResult();
+        session.close();
+        
+        
+        List<Towaryzamowienie> towary = null;
+        session = HibernateUtil.getSessionFactory().openSession();
+        String hql3 = "From Towaryzamowienie WHERE ZamowienieID  = '" + zamowienieID + "'";
+        query = session.createQuery(hql3);
+        towary = query.list();
+        session.close();
+       String[][] productss =  new String[towary.size()][3];
+       
+       
+       
+       
+       
+       System.out.println(towary.get(0));
+       for(int i = 0; i < towary.size() ; i++){
+           
+           
+           Produkty p = null;
+        session = HibernateUtil.getSessionFactory().openSession();
+        String hql4 = "From Produkty WHERE ProduktID  = '" + towary.get(i).getProduktID() + "'";
+        query = session.createQuery(hql4);
+        p = (Produkty) query.uniqueResult();
+        session.close();
+        
+        
+           productss[i][0] = p.getNazwa();
+           productss[i][1] = String.valueOf(towary.get(i).getIlosc());
+           productss[i][2] = String.valueOf(p.getCenaKupna());
+       }
+       
+       
+       //        INSTRUKCJA DO PDF 
+        pdfCreator pdf = new pdfCreator();
+        pdf.createInvoice(zamowienieID,vat, waluta, k, productss);
+//     
+    }
+}
