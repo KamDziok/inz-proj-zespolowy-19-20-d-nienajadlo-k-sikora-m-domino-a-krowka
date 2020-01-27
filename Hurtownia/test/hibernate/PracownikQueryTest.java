7ba8c0a9f1ec71500a826971774c5ab637a1fe7a
Dawid Nienajadło <dawidnienajadlo96@gmail.com>
2020-01-27 17:17:30 +0100

    pracownikQuery tests finished

--- Hurtownia/test/hibernate/PracownikQueryTest.java
+++ Hurtownia/test/hibernate/PracownikQueryTest.java
@@ -37,156 +37,133 @@ public class PracownikQueryTest {
     @After
     public void tearDown() {
     }
+    
+    /**
+     * @author Dawid
+     */
+    
+    @Before
+    public void changeTestPasswordToDefaultValue(){
+        PracownikQuery instance = new PracownikQuery();
+        String login = "kierownik";
+        String password = "kierownik";
+        instance.changePassword(login, password);
+        instance.dodanieDanych("ksiegowa", "ksiegowa", 8);
+    }
 
     /**
      * Test of selecyByLoginandPassword method, of class PracownikQuery.
+     * @author Dawid
      */
     @Test
     public void testSelecyByLoginandPassword() {
         System.out.println("selecyByLoginandPassword");
-        String login = "";
-        String password = "";
+        String login = "kierownik";
+        String password = "kierownik";
         PracownikQuery instance = new PracownikQuery();
-        boolean expResult = false;
         boolean result = instance.selecyByLoginandPassword(login, password);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertTrue(result);
     }
 
     /**
      * Test of selectByLoginandPassword method, of class PracownikQuery.
+     * @author Dawid
      */
     @Test
     public void testSelectByLoginandPassword() {
         System.out.println("selectByLoginandPassword");
-        String login = "";
-        String password = "";
+        String login = "kierownik";
+        String password = "kierownik";
         PracownikQuery instance = new PracownikQuery();
-        Pracownik expResult = null;
+        Pracownik expResult = new Pracownik();
+        expResult.setLogin(login);
         Pracownik result = instance.selectByLoginandPassword(login, password);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(expResult.getLogin(), result.getLogin());
     }
 
     /**
      * Test of wyszukiwanie method, of class PracownikQuery.
+     * @author Dawid
      */
     @Test
     public void testWyszukiwanie() {
         System.out.println("wyszukiwanie");
-        String login = "";
+        String login = "kierownik";
         PracownikQuery instance = new PracownikQuery();
-        Pracownik expResult = null;
+        Pracownik expResult = new Pracownik();
+        expResult.setLogin(login);
         Pracownik result = instance.wyszukiwanie(login);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(expResult.getLogin(), result.getLogin());
     }
 
     /**
      * Test of changePassword method, of class PracownikQuery.
+     * @author Dawid
      */
     @Test
     public void testChangePassword() {
         System.out.println("changePassword");
-        String login = "";
-        String password = "";
+        String login = "kierownik";
+        String password = "kierownik1";
         PracownikQuery instance = new PracownikQuery();
         instance.changePassword(login, password);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        boolean result = instance.selecyByLoginandPassword(login, password);
+        assertTrue(result);
     }
 
     /**
      * Test of PracownikSelectAll method, of class PracownikQuery.
+     * @author Dawid
      */
     @Test
     public void testPracownikSelectAll() {
         System.out.println("PracownikSelectAll");
         PracownikQuery instance = new PracownikQuery();
-        List<Pracownik> expResult = null;
         List<Pracownik> result = instance.PracownikSelectAll();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertNotNull(result);
     }
-
-    /**
-     * Test of changeProductQuantity method, of class PracownikQuery.
-     */
-   
-    /**
-     * Test of addNewProduct method, of class PracownikQuery.
-     */
-    @Test
-    public void testAddNewProduct() throws Exception {
-        System.out.println("addNewProduct");
-        int productID = 0;
-        String productName = "";
-        double productPrice = 0.0;
-        String productDescription = "";
-        int categoryID = 0;
-        PracownikQuery instance = new PracownikQuery();
-        instance.addNewProduct(productID, productName, productPrice, productDescription, categoryID);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of addNewAdvert method, of class PracownikQuery.
-     */
- 
-
-    /**
-     * Test of removeAdvert method, of class PracownikQuery.
-     */
  
     /**
      * Test of wyszukiwanieID method, of class PracownikQuery.
+     * @author Dawid
      */
     @Test
     public void testWyszukiwanieID() {
         System.out.println("wyszukiwanieID");
-        int id = 0;
+        int id = 3;
         PracownikQuery instance = new PracownikQuery();
-        Pracownik expResult = null;
         Pracownik result = instance.wyszukiwanieID(id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertNotNull(result);
     }
 
     /**
      * Test of wyszukanieZatrudnienie method, of class PracownikQuery.
+     * @author Dawid
      */
     @Test
     public void testWyszukanieZatrudnienie() {
         System.out.println("wyszukanieZatrudnienie");
-        String nazwisko = "";
-        String imie = "";
+        String nazwisko = "Jan";
+        String imie = "Kowalski";
         PracownikQuery instance = new PracownikQuery();
-        Pracownik expResult = null;
         Pracownik result = instance.wyszukanieZatrudnienie(nazwisko, imie);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertNotNull(result);
     }
 
     /**
      * Test of dodanieDanych method, of class PracownikQuery.
+     * @author Dawid
      */
     @Test
     public void testDodanieDanych() {
         System.out.println("dodanieDanych");
-        String login = "";
-        String password = "";
-        int id = 0;
+        String login = "test";
+        String password = "test";
+        int id = 8;
         PracownikQuery instance = new PracownikQuery();
         instance.dodanieDanych(login, password, id);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        boolean result = instance.selecyByLoginandPassword(login, password);
+        assertTrue(result);
     }
     
 }
