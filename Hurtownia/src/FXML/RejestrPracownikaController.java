73aae4d918050bae5f898e4c94922f0628404225
Dawid Nienajadło <dawidnienajadlo96@gmail.com>
2020-02-02 03:24:55 +0100

    register panel fixed, remind password prepared to fixing

--- Hurtownia/src/FXML/RejestrPracownikaController.java
+++ Hurtownia/src/FXML/RejestrPracownikaController.java
@@ -1,24 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package FXML;
 
 import Utils.Popup;
-import com.jfoenix.controls.JFXButton;
-import hibernate.Pracownik;
 import hibernate.PracownikQuery;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
 
 /**
  * FXML Controller class
@@ -28,31 +18,13 @@ import javafx.scene.layout.Pane;
 public class RejestrPracownikaController extends Logowanie implements
         Initializable {
 
-
-    @FXML
-    private Button sprawdzW;
-    @FXML
-    private JFXButton powrotW;
-    @FXML
-    private Label weryfikacja;
-    @FXML
-    private Label IDweryf;
     @FXML
     private TextField loginR;
     @FXML
     private PasswordField hasloR;
     @FXML
-    private Button zarejestrujbtn;
-    @FXML
-    private TextField checkNazwisko;
-    @FXML
-    private TextField checkImie;
-    @FXML
     private TextField workerCode;
 
-    /**
-     * Initializes the controller class.
-     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
@@ -65,6 +37,12 @@ public class RejestrPracownikaController extends Logowanie implements
         wczytywanie(event, powrot);
         ramka(event);
     }
+    
+    private void clearFields(){
+        loginR.setText("");
+        hasloR.setText("");
+        workerCode.setText("");
+    }
 
     @FXML
     private void zarejestruj(ActionEvent event) throws Exception {
@@ -74,9 +52,11 @@ public class RejestrPracownikaController extends Logowanie implements
         int code = Integer.parseInt(workerCode.getText());
         
         if(pracownik.wyszukiwanieID(code).getLogin() != null){
+            clearFields();
             Popup.show("Nie można zarejestrować się z tym kodem, ponieważ zostal on już użyty!");
             throw new Exception();
         } else if(pracownik.wyszukiwanieID(code).getPassword() != null){
+            clearFields();
             Popup.show("Nie można zarejestrować się z tym kodem, ponieważ zostal on już użyty!");
             throw new Exception();
         }
@@ -88,8 +68,9 @@ public class RejestrPracownikaController extends Logowanie implements
             ramka(event);
             Popup.show("Rejestracja przebiegła pomyslnie! Możesz się teraz zalogować.");
         }catch(Exception e){
+            loginR.setText("");
+            hasloR.setText("");
             Popup.show("Nie można zalogować się z tym kodem!");
-            System.out.println(e.getMessage());
         }
     }
 
