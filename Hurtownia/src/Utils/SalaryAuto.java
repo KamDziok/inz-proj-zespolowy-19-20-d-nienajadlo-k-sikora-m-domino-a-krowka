0527bbf5683abc939044bdbcfb61bab8a2103b8e
Dawid Nienajadło <dawidnienajadlo96@gmail.com>
2020-02-02 23:00:59 +0100

    auto salaries at 10th day of month added

--- Hurtownia/src/Utils/SalaryAuto.java
+++ Hurtownia/src/Utils/SalaryAuto.java
@@ -17,22 +17,29 @@ public class SalaryAuto {
     private static WyplatyQuery salariesQuery = new WyplatyQuery();
     public static PracownikQuery workersQuery = new PracownikQuery();
     
-    public static boolean autoAddSalary(){
+    public static boolean autoAddSalary() throws Exception{
         Calendar actualCalendar = Calendar.getInstance();
         List<Pracownik> workers;
         int dayOfMonth = actualCalendar.get(Calendar.DAY_OF_MONTH);
         Date date = new Date();
         String dateAsString = date.toString();
         SimpleDateFormat formatDate = new SimpleDateFormat("yyyy-MM-dd");
+        String dateOfSalary = formatDate.format(date).toString();
         
-        if(dayOfMonth == 2){
-            System.err.println("Jest 2");
+        List<Wyplaty> salariesAll = salariesQuery.WyplatySelectAll();
+        
+        if(!salariesAll.isEmpty() && salariesAll.get(0).getData().getMonth()+1 == date.getMonth()+1){
+            System.err.println("Dodałeś wypłaty ");
+            return false;
+        }
+        
+        if(dayOfMonth == 10){
             workers = workersQuery.PracownikSelectAll();
             for(Pracownik worker : workers){
-                salariesQuery.dodajWyplate(dateAsString, worker.getPlaca(), worker.getPracownikId());
+                salariesQuery.dodajWyplate(dateOfSalary, worker.getPlaca(), worker.getPracownikId());
             }
         } else{
-            System.err.println("Nie jest 2 bo jest " + dayOfMonth);
+            System.err.println("Nie można dodać wypłat pracowników!");
         }
         
         return true;
